bbe4dc975cec74b30776d2d03a178061f59bde43
pfent <philipp@fent.de>
2017-03-14 14:45:17 +0100

    get all settings from settings also do a more consistent formatting

--- src/main/java/nl/tudelft/tud15a/snake/model/Apple.java
+++ src/main/java/nl/tudelft/tud15a/snake/model/Apple.java
@@ -1,32 +1,25 @@
 package nl.tudelft.tud15a.snake.model;
 
-import javax.swing.ImageIcon;
-
 public class Apple {
     private Position position;
 
-    private final int CELL_SIZE = 10;
     private int boardSize = Settings.WIDTH;
 
     public Apple() {
-    	position = new Position();
-    	locate();
-    	boardSize = Settings.WIDTH;
-    	
-    	
+        position = new Position();
+        locate();
     }
 
     public Position getPosition() {
-    	return position;
+        return position;
     }
 
     public void locate() {
-        int r = 1 + (int)(Math.random()*(boardSize/CELL_SIZE-2));
-        position.setX(r*CELL_SIZE);
-        
-        
+        int r = 1 + (int) (Math.random() * (boardSize / Settings.CELL_SIZE - 2));
+        position.setX(r * Settings.CELL_SIZE);
 
-        r = 1+(int)Math.round((Math.random()*(boardSize/CELL_SIZE-2)));
-        position.setY(r*CELL_SIZE);
+
+        r = 1 + (int) Math.round((Math.random() * (boardSize / Settings.CELL_SIZE - 2)));
+        position.setY(r * Settings.CELL_SIZE);
     }
 }
--- src/main/java/nl/tudelft/tud15a/snake/model/Model.java
+++ src/main/java/nl/tudelft/tud15a/snake/model/Model.java
@@ -1,21 +1,13 @@
 package nl.tudelft.tud15a.snake.model;
 
-import javax.swing.ImageIcon;
-import javax.swing.Timer;
-
 public class Model {
     Snake snake;
     private Apple apple;
     private State inGame = State.START_SCREEN;
-    private int borderThickness=5;
-    private ImageIcon ic;
 
     public Model() {
-
         snake = new Snake();
         apple = new Apple();
-        ic = new ImageIcon("src/main/java/nl/tudelft/tud15a/snake/view/images/body.jpg");
-        borderThickness = ic.getIconHeight();
     }
 
     public void checkApple() {
@@ -29,48 +21,40 @@ public class Model {
     }
 
     public void checkCollision() {
-
-        if(snake.isEatingYourself()) {
-        	inGame = State.GAME_OVER;;
-        }
-
-
-        if (snake.getHead().getY() >= Settings.HEIGHT-this.borderThickness) {
-            inGame = State.GAME_OVER;
-
-        }
-
-        if (snake.getHead().getY() < this.borderThickness) {
-            inGame = State.GAME_OVER;
-
-        }
-
-
-        if (snake.getHead().getX() >= Settings.WIDTH-this.borderThickness) {
-            inGame = State.GAME_OVER;
-
-
-        }
-
-        if (snake.getHead().getX() < this.borderThickness) {
+        if (snake.isEatingYourself()) {
             inGame = State.GAME_OVER;
         }
 
+        if (snake.getHead().getY() >= Settings.HEIGHT - Settings.BORDER_THICKNESS) {
+            inGame = State.GAME_OVER;
+        }
+
+        if (snake.getHead().getY() < Settings.BORDER_THICKNESS) {
+            inGame = State.GAME_OVER;
+        }
+
+        if (snake.getHead().getX() >= Settings.WIDTH - Settings.BORDER_THICKNESS) {
+            inGame = State.GAME_OVER;
+        }
+
+        if (snake.getHead().getX() < Settings.BORDER_THICKNESS) {
+            inGame = State.GAME_OVER;
+        }
     }
 
     public State getState() {
-    	return inGame;
+        return inGame;
     }
 
     public void setState(State state) {
-    	inGame = state;
+        inGame = state;
     }
 
     public Snake getSnake() {
-    	return snake;
+        return snake;
     }
 
     public Apple getApple() {
-    	return apple;
+        return apple;
     }
 }
