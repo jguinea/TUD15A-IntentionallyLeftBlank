f311c5a382a47042f88e1b77fd3f554e96a66243
Sebastian Stobiecki <sebekstobiecki@gmail.com>
2017-03-14 02:09:56 +0100

    Basic Snake

---
+++ src/main/java/nl/tudelft/tud15a/snake/model/Constants.java
@@ -0,0 +1,9 @@
+package nl.tudelft.tud15a.snake.model;
+
+public class Constants {  
+    public final static int WIDTH = 300;
+    public final static int HEIGHT = 300;
+    public final static int CELL_SIZE = 10;
+    public final static int ALL_CELLS = 900;
+    public final static int DELAY = 150;
+}
---
+++ src/main/java/nl/tudelft/tud15a/snake/model/Direction.java
@@ -0,0 +1,5 @@
+package nl.tudelft.tud15a.snake.model;
+
+public enum Direction {
+    LEFT, RIGHT, UP, DOWN;
+}
