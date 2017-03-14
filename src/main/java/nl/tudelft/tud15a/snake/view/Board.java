d5bcdc2d0166ad6ef3406ae7af6ade3bc8abd496
Sebastian Stobiecki <sebekstobiecki@gmail.com>
2017-03-14 12:21:03 +0100

    add view package

--- src/main/java/nl/tudelft/tud15a/snake/view/Board.java
+++ src/main/java/nl/tudelft/tud15a/snake/view/Board.java
@@ -1,11 +1,25 @@
-package nl.tudelft.tud15a.snake.model;
-
-import javax.swing.*;
-import java.awt.*;
+package nl.tudelft.tud15a.snake.view;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import nl.tudelft.tud15a.snake.model.Direction;
+import nl.tudelft.tud15a.snake.model.Model;
+import nl.tudelft.tud15a.snake.model.Position;
+import nl.tudelft.tud15a.snake.model.Settings;
+import nl.tudelft.tud15a.snake.model.State;
 
 public class Board extends JPanel implements ActionListener {
     Model model = new Model();
