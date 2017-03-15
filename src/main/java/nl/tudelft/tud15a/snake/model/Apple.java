267eba59a98b3d4d54ef3ce52610f15c4f91ee2d
Soso <sofianehorache@yahoo.fr>
2017-03-15 10:00:11 +0100

    fix the problem of random position of Apple

--- src/main/java/nl/tudelft/tud15a/snake/model/Apple.java
+++ src/main/java/nl/tudelft/tud15a/snake/model/Apple.java
@@ -15,11 +15,11 @@ public class Apple {
     }
 
     public void locate() {
-        int r = 1 + (int) (Math.random() * (boardSize / Settings.CELL_SIZE - 2));
+        int r = 1 + (int) (1 * (boardSize / Settings.CELL_SIZE - 3));
         position.setX(r * Settings.CELL_SIZE);
 
 
-        r = 1 + (int) Math.round((Math.random() * (boardSize / Settings.CELL_SIZE - 2)));
+        r = 1 + (int) Math.round((0.999999999* (boardSize / Settings.CELL_SIZE - 2)));
         position.setY(r * Settings.CELL_SIZE);
     }
 }
